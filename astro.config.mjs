738bb1c55dd6bbfdd85601359302332ec1dbe338
Jhoncel Nayve <jhoncel.nayve@codedepot.ch>
2025-06-05 14:08:23 +0000

    Install and setup preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     host: true,
   },
   site: "https://localhost:4321",
+  integrations: [preact()],
 });
